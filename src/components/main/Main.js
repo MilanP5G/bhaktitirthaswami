9903a6b31fa7004633fc93a46157434c6a322e32
MilanP5G <milan_parmar@hotmail.co.uk>
2021-07-15 09:21:49 +0100

    added main image for central introduction

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import mainImage from '../../assets/BhaktiTirthaSwamiMain.png'
 
 const Main = () => {
+
   return (
+    <div className='origin-box'>
+      <div className='origin-image'>
+        <img alt='Bhakti Tirtha Swami' src={mainImage} />
+      </div>
+    </div>
 
   )
+
 }
 
 export default Main;
