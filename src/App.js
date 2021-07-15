bb730443756c714c7325d75d38d184cdf2cb61f9
MilanP5G <milan_parmar@hotmail.co.uk>
2021-07-15 09:23:02 +0100

    imported and included Main component into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Main from './components/main/Main';
 import NavBar from './components/navbar/NavBar';
 import './App.css';
 
@@ -9,10 +10,11 @@ const App = () => {
     <Router>
       <div>
         <NavBar />
+        <Main />
       </div>
     </Router>
   )
-  
+
 }
 
 export default App;
